b1f86dcfb989fc31750dbc5de7bf2862c6dd0573
mac <khadijatououmytraore@esp.sn>
2026-05-03 01:43:36 +0000

    Lab 3 : API FastAPI connectee au modele ML (v2)

---
+++ api/main.py
@@ -0,0 +1,87 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import joblib
+import numpy as np
+
+# =========================
+# Charger les fichiers ML
+# =========================
+
+model = joblib.load("models/model.pkl")
+
+encoder_sexe = joblib.load("models/encoder_sexe.pkl")
+encoder_region = joblib.load("models/encoder_region.pkl")
+
+feature_cols = joblib.load("models/feature_cols.pkl")
+
+# =========================
+# Créer l'application
+# =========================
+
+app = FastAPI()
+
+# =========================
+# Route accueil
+# =========================
+
+@app.get("/")
+def home():
+    return {"message": "API SenSante active"}
+
+# =========================
+# Schéma patient
+# =========================
+
+class PatientData(BaseModel):
+    age: int
+    sexe: str
+    temperature: float
+    tension_sys: int
+    toux: bool
+    fatigue: bool
+    maux_tete: bool
+    frissons: bool
+    nausee: bool
+    region: str
+
+# =========================
+# Endpoint prediction
+# =========================
+
+@app.post("/predict")
+def predict(data: PatientData):
+
+    # Encoder les données
+    sexe_encoded = encoder_sexe.transform([data.sexe])[0]
+    region_encoded = encoder_region.transform([data.region])[0]
+
+    # Construire les features
+    features = [[
+        data.age,
+        sexe_encoded,
+        data.temperature,
+        data.tension_sys,
+        int(data.toux),
+        int(data.fatigue),
+        int(data.maux_tete),
+        int(data.frissons),
+        int(data.nausee),
+        region_encoded
+    ]]
+
+    # Faire la prédiction
+    prediction = model.predict(features)[0]
+
+    # Probabilités
+    probabilities = model.predict_proba(features)[0]
+
+    # Construire réponse
+    resultats = {}
+
+    for classe, proba in zip(model.classes_, probabilities):
+        resultats[classe] = round(float(proba), 4)
+
+    return {
+        "diagnostic": prediction,
+        "probabilites": resultats
+    }
